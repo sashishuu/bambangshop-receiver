844d02defa86f7799748456163c53415f8507ac7
sashishuu <emailkamu@gmail.com>
2025-03-28 21:22:52 +0700

    Implement receive notification function in Notification controller and update routes.

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -6,3 +6,10 @@ pub fn route_stage() -> AdHoc {
     });
 }
 
+pub fn route_stage() -> AdHoc {
+    return AdHoc::on_ignite("Initializing controller routes...", |rocket| async {
+        rocket
+            .mount("/", routes![notification::subscribe, notification::unsubscribe, notification::receive])
+    });
+}
+
