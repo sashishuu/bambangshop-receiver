2fc01016249f5c69a49b893c221accee687c05a6
sashishuu <emailkamu@gmail.com>
2025-03-28 21:22:08 +0700

    Implement receive_notification function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -93,6 +93,11 @@ pub fn unsubscribe(product_type: &str) -> Result<SubscriberRequest> {
         .join().unwrap();
 }
 
+pub fn receive_notification(payload: Notification) -> Result<Notification> {
+    let subscriber_result: Notification = NotificationRepository::add(payload);
+    return Ok(subscriber_result);
+}
+
 
 }
 
